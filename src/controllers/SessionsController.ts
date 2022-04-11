42b6a3596a39d58af1481da8efc15844944f13e4
unknown <juliostdfac@gmail.com>
2022-04-11 07:24:23 -0300

    debug user

--- src/controllers/SessionsController.ts
+++ src/controllers/SessionsController.ts
@@ -5,32 +5,35 @@ import User from "../models/user";
 import { validatePass } from "../services/authentication/encrypt";
 
 class SessionsController {
-  
   async create(req: Request, res: Response) {
     try {
-      const { email, password } =  req.body;
-      const user = await User.findOne({ email }) // same as email: email
-  
-      if (!user) 
-        return res.status(401).json({ message: "Invalid email or password", status: "failure" })
-      if (!await validatePass(user, password)) 
-        return res.status(401).json({ message: "Invalid email or password", status: "failure" })
-    
-      const { id } = user
-  
+      const { email, password } = req.body;
+      const user = await User.findOne({ email }); // same as email: email
+      console.log(user);
+      if (!user)
+        return res
+          .status(401)
+          .json({ message: "Invalid email or password", status: "failure" });
+      if (!(await validatePass(user, password)))
+        return res
+          .status(401)
+          .json({ message: "Invalid email or password", status: "failure" });
+
+      const { id } = user;
+
       return res.json({
         user: {
           id,
-          email
+          email,
         },
         token: jwt.sign({ id }, authConfig.secret, {
-          expiresIn: authConfig.expiresIn
-        })
-      })
+          expiresIn: authConfig.expiresIn,
+        }),
+      });
     } catch (e) {
-      return res.status(500).json({})
+      return res.status(500).json({});
     }
   }
 }
 
-export default new SessionsController()
+export default new SessionsController();
